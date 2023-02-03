c20c54229395d4a615d602b272bceea32b9e43c3
Mo3-dev <moisesb2001@gmail.com>
2023-02-03 00:17:53 -0400

    adj form

--- responsive_functional_form/form.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-    let x = document.querySelector('.input');
-
-    if (x == "") {
-      alert("Por favor rellene los campos correctamente");
-      return false;
-    }
-  }
